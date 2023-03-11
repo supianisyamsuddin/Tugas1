e2261f1ae6b3f42d4abe202a66f46b7b9d4f9923
supianisyamsuddin <127589994+supianisyamsuddin@users.noreply.github.com>
2023-03-11 20:08:34 +0800

    Create Waktu.java

---
+++ Tugas1_SourceCode_Kasus/Waktu.java
@@ -0,0 +1,33 @@
+ /*	NIM   		: 13020210085
+	Nama   		: Supiani Syamsuddin
+	Hari/Tanggal	: Jumat, 6 Maret 2023
+	Waktu Pengerjaan  : 20.19 */
+
+
+import java.util.Scanner;
+
+public class Waktu {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int totJam, jam, totMen, menit, detik, konversi;
+
+        System.out.print("Masukkan total detik : ");
+        konversi = input.nextInt();
+
+        detik = konversi % 60;
+        totMen = konversi / 60;
+        menit = totMen % 60;
+        totJam = totMen / 60;
+        jam = totJam % 24;
+        
+        System.out.println("Detik sekarang  : " +detik+ " detik");
+        System.out.println("Total menit     : " +totMen+ " menit");
+        System.out.println("Menit sekarang  : " +menit+ " menit");
+        System.out.println("Total jam       : " +totJam+ " jam");
+        System.out.println("Jam sekarang    : " +jam+ " jam");
+        System.out.println("Tampil waktu    : " + jam + ":" + menit + ":" + detik);
+    }
+    
+}
